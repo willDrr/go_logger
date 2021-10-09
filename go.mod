cb2848bd3ebc811723f973e8daa788e44513af41
willDrr <willxdd97@gmail.com>
2021-10-08 20:40:20 -0600

    Adding internal directory

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/willDrr/proglog
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
---
+++ internal/log/store_test.go
@@ -0,0 +1,31 @@
+package log
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	write = []byte("Hello world")
+	width = uint64(len(write)) + lenWidth
+)
+
+func TestStoreAppendRead(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_append_read_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	testAppend(t, s)
+	testRead(t, s)
+	testRead(t, s)
+
+	s, err = newStore(f)
+	require.NoError(t, err)
+	testRead(t, s)
+}
